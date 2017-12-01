ba1bc7d7c659a2557e0870bb1000c2044e8e7419
Rajhqi116v <githandreysg56d@yandex.ru>
2017-12-01 19:53:21 +0300

    jduyjuyjuykd

--- js/contact_me.js
+++ js/contact_me.js
@@ -33,7 +33,7 @@ $(function() {
             	   $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
             		.append( "</button>");
             	  $('#success > .alert-success')
-            		.append("<strong>Your message has been sent. </strong>");
+            		.append("<strong>Ваше сообщение отправлено </strong>");
  		  $('#success > .alert-success')
  			.append('</div>');
  						    
@@ -45,7 +45,7 @@ $(function() {
  		 $('#success').html("<div class='alert alert-danger'>");
             	$('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
             	 .append( "</button>");
-            	$('#success > .alert-danger').append("<strong>Sorry "+firstName+" it seems that my mail server is not responding...</strong> Could you please email me directly to <a href='mailto:me@example.com?Subject=Message_Me from myprogrammingblog.com'>me@example.com</a> ? Sorry for the inconvenience!");
+            	$('#success > .alert-danger').append("<strong>Sorry "+firstName+" Похоже, сервер не отвечает</strong> Напишите мне напрямую на этот адрес gswebiiisg2@yandex.ru );
  	        $('#success > .alert-danger').append('</div>');
  		//clear all fields
  		$('#contactForm').trigger("reset");
